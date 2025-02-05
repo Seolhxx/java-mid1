2bc8c278a406d968ab2abbe691ac8854f5888bd1
apffhel <xxrosevly@gmail.com>
2025-02-05 20:12:02 +0900

    2025.02.05 2.불변 객체 (문제풀이)

--- src/lang/immutable/test/ImmutableMyDate.java
+++ src/lang/immutable/test/ImmutableMyDate.java
@@ -2,8 +2,8 @@ package lang.immutable.test;
 
 public class ImmutableMyDate {
     private final int year;
-    private int month;
-    private int day;
+    private final int month;
+    private final int day;
 
     public ImmutableMyDate(int year, int month, int day) {
         this.year = year;
@@ -11,21 +11,17 @@ public class ImmutableMyDate {
         this.day = day;
     }
 
-    public ImmutableMyDate(int year) {
-        this.year = year;
+    public ImmutableMyDate withYear(int newYear) {
+        return new ImmutableMyDate(newYear, month, day);
+    }
+
+    public ImmutableMyDate withMonth(int newMonth) {
+        return new ImmutableMyDate(year, newMonth, day);
     }
 
 
-    public ImmutableMyDate setYear(int year) {
-        return new ImmutableMyDate(year);
-    }
-
-    public void setMonth(int month) {
-        this.month = month;
-    }
-
-    public void setDay(int day) {
-        this.day = day;
+    public ImmutableMyDate withDay(int newDay) {
+        return new ImmutableMyDate(year, month, newDay);
     }
 
     @Override
