8f22357ac461441cc87be11d1e9fc30de3e5807f
apffhel <xxrosevly@gmail.com>
2025-02-06 16:23:31 +0900

    2025.02.05 3.String 클래스

---
+++ src/lang/string/CharArrayMain.java
@@ -0,0 +1,12 @@
+package lang.string;
+
+public class CharArrayMain {
+
+    public static void main(String[] args) {
+        char a = '가'; //문자 하나만
+        System.out.println("a = " + a);
+        char[] charArr = new char[]{'h', 'e', 'h'};
+        System.out.println(charArr); //여러 문자열 char로 담으려면 배열로
+
+    }
+}
---
+++ src/lang/string/StringBasicMain.java
@@ -0,0 +1,12 @@
+package lang.string;
+
+public class StringBasicMain {
+
+    public static void main(String[] args) {
+        String str1 = "hello";
+        String str2 = new String("hello"); //문자도 객체 생성 가능
+
+        System.out.println("str1 = " + str1);
+        System.out.println("str2 = " + str2);
+    }
+}
