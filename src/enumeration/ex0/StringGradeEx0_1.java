08f10aeaf1f61250623bdaee259f27e7a938609a
apffhel <xxrosevly@gmail.com>
2025-02-08 22:05:00 +0900

    2025.02.08 5.열거형 - ENUM - 문자열과 타입 안전성

---
+++ src/enumeration/ex0/StringGradeEx0_1.java
@@ -0,0 +1,17 @@
+package enumeration.ex0;
+
+public class StringGradeEx0_1 {
+
+    public static void main(String[] args) {
+        int price = 10000;
+
+        DiscountService discountService = new DiscountService();
+        int basic = discountService.disciunt("BASIC", price);
+        int gold = discountService.disciunt("GOLD", price);
+        int diamond = discountService.disciunt("DIAMOND", price);
+
+        System.out.println("BASIC 등급의 할인 금액: " + basic);
+        System.out.println("GOLD 등급의 할인 금액: " + gold);
+        System.out.println("DIAMOND 등급의 할인 금액: " + diamond);
+    }
+}
