9b9253aad75764bd90c66a229056797e183ac568
apffhel <xxrosevly@gmail.com>
2025-02-11 19:00:48 +0900

    2025.02.11 6.날짜와 시간 - ZonedDateTime

---
+++ src/time/ZoneIdMain.java
@@ -0,0 +1,22 @@
+package time;
+
+import java.time.ZoneId;
+
+public class ZoneIdMain {
+
+    public static void main(String[] args) {
+        for (String availableZoneId : ZoneId.getAvailableZoneIds()) {
+            System.out.println("availableZoneId = " + availableZoneId);
+            //객체로 얻기
+            ZoneId zoneId = ZoneId.of(availableZoneId);
+            System.out.println(zoneId + " | " + zoneId.getRules());
+        }
+
+        ZoneId zoneId = ZoneId.systemDefault();
+        System.out.println("ZoneId.systemDefault = " + zoneId);
+
+        ZoneId seoulZoneId = ZoneId.of("Asia/Seoul");
+        System.out.println("seoulZoneId = " + seoulZoneId);
+
+    }
+}
