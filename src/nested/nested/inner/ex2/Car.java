626f12efb13f29636c12d181dfc041df59c1c2f9
apffhel <xxrosevly@gmail.com>
2025-02-19 15:36:34 +0900

    2025.02.19 7.중첩 클래스, 내부 클래스1 - 내부 클래스의 활용

---
+++ src/nested/nested/inner/ex2/Car.java
@@ -0,0 +1,33 @@
+package nested.nested.inner.ex2;
+
+public class Car {
+    private String model;
+    private int chargeLevel;
+    private Engine engine;
+
+    public Car(String model, int chargeLevel) {
+        this.model = model;
+        this.chargeLevel = chargeLevel;
+        this.engine = new Engine();
+    }
+
+    public void start() {
+        engine.start();
+        System.out.println(model + " 시작 완료");
+    }
+
+    private class Engine {
+
+        //바깥 클래스의 인스턴스에 접근할 수 있으므로 이 정보 필요 없음
+       /* private Car car;
+
+        public Engine(Car car) {
+            this.car = car;
+        }*/
+
+        public void start() {
+            System.out.println("충전 레벨 확인: " + chargeLevel);
+            System.out.println(model + "의 엔진을 구동합니다.");
+        }
+    }
+}
