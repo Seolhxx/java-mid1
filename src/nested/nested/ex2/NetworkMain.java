9d3d43abf60220af5836d9caee25b86250bb4d2c
apffhel <xxrosevly@gmail.com>
2025-02-20 20:35:19 +0900

    2025.02.20

--- src/nested/nested/ex2/NetworkMain.java
+++ src/nested/nested/ex2/NetworkMain.java
@@ -1,10 +1,9 @@
-package nested.ex1;
+package nested.nested.ex2;
 
 public class NetworkMain {
 
     public static void main(String[] args) {
         Network network = new Network();
         network.sendMessage("hello java");
-
     }
 }
