e07635d363f382901a6fed9029745cadb9ddfcda
apffhel <xxrosevly@gmail.com>
2025-03-01 23:36:51 +0900

    2025.03.01 8.중첩 클래스, 내부 클래스2 - 문제풀이 전

---
+++ src/nested/test/OuterClass1.java
@@ -0,0 +1,9 @@
+package nested.test;
+
+public class OuterClass1 {
+
+    public void hello() {
+        System.out.println("OuterClass1.hello");
+    }
+
+}
---
+++ src/nested/test/OuterClass1Main.java
@@ -0,0 +1,8 @@
+package nested.test;
+
+public class OuterClass1Main {
+    public static void main(String[] args) {
+        OuterClass1 outer = new OuterClass1();
+        outer.hello();
+    }
+}
